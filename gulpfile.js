4e783cf11d215c31f8f65b13a61339a3b760d9eb
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-08-16 11:36:45 -0300

    nav

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var watch = require('gulp-watch');
+var webserver = require('gulp-webserver');
+
+gulp.task('script', function(){
+	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap/dist/js/bootstrap.js',
+		'assets/js/*js'])
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('style', function(){
+	gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css', 'assets/sass/main.scss'])
+	.pipe(sass().on('error', sass.logError))
+	.pipe(minifyCSS())
+	.pipe(concat('style.min.css'))
+	.pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('assets/sass/*.scss', ['style']);
+	gulp.watch('assets/js/*js', ['script']);
+})
+
+gulp.task('webserver', function(){
+	gulp.src('../tasty-food/')
+	.pipe(webserver({
+		fallback: 'index.html',
+		livereload: true,
+		directoryListing: false,
+		open: true,
+		port: 8001
+	}));
+});
+gulp.task('default', ['script','style', 'webserver', 'watch']);
