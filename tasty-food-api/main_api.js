3952bb413cd70a7f586f7b787e9db38b44c61085
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-08-16 17:13:48 -0300

    se rescata toda la info necesaria desde la api

---
+++ tasty-food-api/main_api.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+	$(".search").click(function(){
+
+		var idCity = $("select").val();
+
+		$.ajax({
+			url: 'https://developers.zomato.com/api/v2.1/search',
+			beforeSend: function(req) {
+				req.setRequestHeader('user-key','4540d992a1972a96c9d8e233c5d69e3d');
+			},
+			type: 'GET',
+			dataType: 'json',
+			data: {
+				entity_id: idCity,
+				entity_type: 'city'
+			}
+		})
+		.done(function(response) {
+			console.log("success");
+			response.restaurants.forEach(function(e){
+				$(".contenedor").append('<p>nombre: ' + e.restaurant.name + '</p>' + '<p>direccion: ' + e.restaurant.location.address + '</p>' + '<p>comuna: ' + e.restaurant.location.locality + '</p>' + '<img src="' + e.restaurant.thumb + '">' + '</p>' + '<p>precio por dos: ' + e.restaurant.average_cost_for_two + '</p>' + '<p>rating: ' + e.restaurant.user_rating.aggregate_rating + '</p>' + '<p>cuisine: ' + e.restaurant.cuisines + '</p>' + '<hr>');
+			})
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+	});
+
+})
