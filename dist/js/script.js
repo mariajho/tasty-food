a785f8be9270d46b6a87f012f0e03952b9ccc51b
esosdiaz <diazgonzalez.perla@gmail.com>
2017-08-17 08:33:51 -0300

    maquetacion create account

--- dist/js/script.js
+++ dist/js/script.js
@@ -12631,31 +12631,32 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 
+
 $(document).ready(function(){ 
 	//toggle del boton login
    $('#btn-logIn').on('click',function(){
       $('#log-In').toggle('slow');
    });
    //validacion login
-  	$('#mensajeUser').hide();
+	$('#mensajeName').hide();
 	$('#mensajePass').hide();
 
 	$('#letsgo').click(function(){
 		validateForm();
 	});
 	function validateForm(){
-		var usuario = $('#user').val();
+		var usuario = $('#name').val();
 		var contrasena = $('#pass').val();
-		var usuarioLocal= localStorage.getItem('userDat');
+		var usuarioLocal= localStorage.getItem('nameDat');
 		var contrasenaLocal=localStorage.getItem('passDat');
 
 		if (usuario == usuarioLocal && contrasena == contrasenaLocal){
 			window.location.href = "profile.html"; 
 				}else{
 					if(usuario == "" || usuario != usuarioLocal){
-						$('#mensajeUser').fadeIn();
+						$('#mensajeName').fadeIn();
 						}else{
-							$('#mensajeUser').hide();
+							$('#mensajeName').hide();
 							if(contrasena == "" || contrasena != contrasenaLocal){
 								$('#mensajePass').fadeIn();
 							}
